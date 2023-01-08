fe7046385d5c3e20e55f0696436427e0c8013219
Agela <ma2991999@gmail.com>
2023-01-08 14:35:21 +0530

    some more cleanup and restructed files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div>Testing something</div>;
-}
+import { Button } from "@mui/material";
+import { Container } from "@mui/system";
+
+const App = () => {
+  const onButtonClick = () => {
+    alert("Clicked a button");
+  };
+  return (
+    <Container>
+      Main page
+      <br />
+      <Button variant="contained" onClick={onButtonClick}>
+        Hell World
+      </Button>
+    </Container>
+  );
+};
 
 export default App;
